1a9abf12ea88cdaa570319a8c508569a4dc6fc7e
Anupam Kumari Mishra <131358927+Anupam2027@users.noreply.github.com>
2025-03-25 12:30:27 +0000

    redux toolkit configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-console.log(import.meta.env.VITE_APPWRITE_URL);
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authservice from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 function App() {
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          ToDo:
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
